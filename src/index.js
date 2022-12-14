c2513e3ff7bc78d199173b41656a8266648150ce
Iryna Grytsaenko <iryna.grytsaenko@gmail.com>
2022-12-14 15:57:02 +0200

    Added HTML, CSS, partially JS
    html markup added;
    css styles added;
    js:
    axios function added;
    js markup function added;

--- src/index.js
+++ src/index.js
@@ -1,54 +1,87 @@
 import './css/styles.css';
-import fetchCountries from './fetchCountries.js';
+import fetchImages from './fetchImages.js';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const DEBOUNCE_DELAY = 300;
 
-const inputRef = document.getElementById('image-box');
-const imageListRef = document.querySelector('.image-list');
-const searchBtnRef = document.querySelector('.search-box-button');
-
+// const inputRef = document.getElementById('search-box');
+const imageListRef = document.querySelector('div.gallery');
+const formRef = document.getElementById('search-form');
+const loadMoreBtn = document.querySelector('.load-more');
+let request = '';
+let page = 1;
+let perPage = 40;
+const params = {
+  q: '',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+  page: '',
+  per_page: perPage,
+};
 function invokeResponseSet(evt) {
+  evt.preventDefault();
   imageListRef.innerHTML = '';
-  const name = evt.target.value.trim();
-  if (name === '') {
+  page = 1;
+  params.page = page;
+  params.q = evt.currentTarget.elements.searchQuery.value.trim();
+  if (params.q === '') {
     return;
   }
-  fetchImages(name)
-    .then(imageData => createMarkup(imageData))
+  console.log(params);
+  renderImages(params);
+}
+function appendMarkup(imageData) {
+  imageListRef.insertAdjacentHTML('beforeend', createMarkup(imageData));
+}
+function renderImages(params) {
+  return fetchImages(params)
+    .then(imageData => {
+      appendMarkup(imageData);
+      if (params.page === 1)
+        return Notify.success(
+          `Hoorray! We found ${imageData.totalHits} images`
+        );
+    })
     .catch(error => {
       if (error.status === 404) {
-        Notify.failure('Oops, there is no country with that name');
+        Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
         imageListRef.innerHTML = '';
         return;
       }
     });
 }
-inputRef.addEventListener('input', debounce(invokeResponseSet, DEBOUNCE_DELAY));
-// function createMarkup(countryData) {
-//   if (countryData.length > 10) {
-//     Notify.info('Too many matches found. Please enter a more specific name.');
-//     return;
-//   } else if (countryData.length < 10 && countryData.length > 1) {
-//     const markup = createSimpleMarkup(countryData);
-//     countryListRef.insertAdjacentHTML('beforeend', markup);
-//   } else {
-//     countryInfoRef.innerHTML = createAdvancedMarkup(countryData);
-//   }
-// }
-// function createSimpleMarkup(countryData) {
-//   return countryData
-//     .map(
-//       country =>
-//         `<li class='country-item'><img src='${country.flags.svg}' width=50/><h2 class='country-item-name--simple'>${country.name.official}</h2></li>`
-//     )
-//     .join('');
-// }
-// function createAdvancedMarkup(country) {
-//   const countryForMarkup = country[0];
-//   const languages = Object.values(countryForMarkup.languages);
-//   return `<div class='image-container'><img src='${countryForMarkup.flags.svg}' width=50/>
-//   <h2 class='country-item-name'>${countryForMarkup.name.official}</h2></div>
-//   <p class='country-item-text'>Capital:<span class='country-item-valuetext'>${countryForMarkup.capital}</span></p>
-//   <p class='country-item-text'>Population:<span class='country-item-valuetext'>${countryForMarkup.population}</span></p>
-//   <p class='country-item-text'>Languages:<span class='country-item-valuetext'>${languages}</span></p>`;
-// }
+formRef.addEventListener('submit', invokeResponseSet);
+loadMoreBtn.addEventListener('click', () => {
+  params.page += 1;
+  renderImages(params);
+});
+function createMarkup(imageData) {
+  console.log(imageData);
+  return imageData.hits
+    .map(
+      image => `<div class="photo-card">
+  <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" height="200" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+      ${image.likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+      ${image.views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+      ${image.comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+      ${image.downloads}
+    </p>
+  </div>
+</div>`
+    )
+    .join('');
+}
